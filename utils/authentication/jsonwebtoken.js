385480ad5fd869d0ea3e83efe89c617affb819b0
Jaime Martínez <jaimemaga1998@gmail.com>
2022-12-02 22:29:32 +0100

    api test

--- utils/authentication/jsonwebtoken.js
+++ utils/authentication/jsonwebtoken.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const getJWT = (userInfo) => {
-    console.log('CLAVE: ', process.env.JWT_SECRET_KEY);
     return jwt.sign(
         {
             id: userInfo._id,
